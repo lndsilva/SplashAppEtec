bf94ba1cf1263e7b4a1638a5ed124e654ff78971
Enoque Leal <enoquefelipe@gmail.com>
2019-11-06 22:11:29 -0300

    Initial commit

---
+++ app/src/main/java/br/com/splashapp/Principal_Activity.java
@@ -0,0 +1,17 @@
+package br.com.splashapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Principal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal_layout);
+
+
+    }
+}
---
+++ app/src/main/java/br/com/splashapp/Splash_Activity.java
@@ -0,0 +1,26 @@
+package br.com.splashapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Activity.this,
+                        Login_Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
